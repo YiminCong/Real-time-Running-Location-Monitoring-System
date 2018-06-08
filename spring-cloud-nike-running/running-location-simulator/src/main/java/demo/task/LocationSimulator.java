18a9db7a8636e168d5ed24ca3ea16efb00d14b72
congyimin <cym7079306@icloud.com>
2018-06-08 09:05:49 -0400

    java project

---
+++ spring-cloud-nike-running/running-location-simulator/src/main/java/demo/task/LocationSimulator.java
@@ -0,0 +1,175 @@
+package demo.task;
+
+import demo.model.*;
+import demo.service.PositionService;
+import demo.support.NavUtils;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class LocationSimulator implements Runnable {
+
+    @Getter
+    @Setter
+    private long id;
+
+    @Setter
+    private PositionService positionSerivce;
+
+    private AtomicBoolean cancel = new AtomicBoolean();
+
+    private double speedInMps;
+
+    private boolean shoudMove;
+    private boolean exportPositionsToMessaging = true;
+    private Integer reportInterval=500;
+
+    @Getter
+    @Setter
+    private PositionInfo currentPosition = null;
+
+    @Setter
+    private List<Leg> legs;
+    private RunnerStatus runnerStatus = RunnerStatus.NONE;
+    private String runningId;
+
+    @Setter
+    private Point startPoint;
+    private Date executionStartTime;
+
+    private MedicalInfo medicalInfo;
+
+    public LocationSimulator(GpsSimulatorRequest gpsSimulatorRequest){
+        this.shoudMove = gpsSimulatorRequest.isMove();
+        this.exportPositionsToMessaging = gpsSimulatorRequest.isExportPositionsToMessaging();
+        this.setSpeed(gpsSimulatorRequest.getSpeed());
+        this.reportInterval = gpsSimulatorRequest.getReportInterval();
+
+        this.runningId = gpsSimulatorRequest.getRunningId();
+        this.runnerStatus = gpsSimulatorRequest.getRunnerStatus();
+        this.medicalInfo = gpsSimulatorRequest.getMedicalInfo();
+    }
+
+
+    @Override
+    public void run() {
+        try{
+            executionStartTime = new Date();
+            if (cancel.get()){
+                destory();
+                return;
+            }
+
+            while(!Thread.interrupted()){
+                long startTime = new Date().getTime();
+
+                if(currentPosition!=null){
+                    if(shoudMove){
+                        moveRunningLocation();
+                        currentPosition.setSpeed(speedInMps);
+                    }
+                    else{
+                        currentPosition.setSpeed(0.0);
+                    }
+
+                    currentPosition.setRunnerStatus(this.runnerStatus);
+
+                    final MedicalInfo medicalInfoToUse;
+
+                    switch (this.runnerStatus){
+                        case SUPPLY_NOW:
+                        case SUPPLY_SOON:
+                        case STOP_NOW:
+                            medicalInfoToUse = this.medicalInfo;
+                            break;
+                        default:
+                            medicalInfoToUse = null;
+                            break;
+                    }
+
+                    final CurrentPosition currentPosition = new CurrentPosition(this.currentPosition.getRunningId(),
+                            new Point(this.currentPosition.getPosition().getLatitude(), this.currentPosition.getPosition().getLongitude()),
+                            this.currentPosition.getRunnerStatus(),
+                            this.currentPosition.getSpeed(),
+                            this.currentPosition.getLeg().getHeading(),
+                            medicalInfoToUse
+                            );
+
+                    // send current position to distribution service via REST API
+                    // @TODO implement positionInfoService
+                    positionSerivce.processPositionInfo(id, currentPosition, this.exportPositionsToMessaging);
+
+                }
+
+                // wait until next position report
+                sleep(startTime);
+            }
+        }
+        catch(InterruptedException ie){
+            destory();
+            return;
+        }
+        destory();
+    }
+
+    void destory(){
+        currentPosition = null;
+    }
+
+    private void sleep(long startTime) throws InterruptedException{
+        long endTime = new Date().getTime();
+        long elapsedTime = endTime - startTime;
+        long sleepTime = reportInterval - elapsedTime > 0 ? reportInterval - elapsedTime : 0 ;
+        Thread.sleep(sleepTime);
+    }
+
+    // Set new position of running location based on current position and running speed
+    private void moveRunningLocation(){
+        double distance  = speedInMps * reportInterval / 1000.0;
+        double distanceFromStart = currentPosition.getDistanceFromStart() + distance;
+        double excess = 0.0;
+
+        for (int i=currentPosition.getLeg().getId(); i<legs.size(); i++){
+            Leg currentLeg = legs.get(i);
+            excess = distanceFromStart > currentLeg.getLength() ? distanceFromStart - currentLeg.getLength() : 0.0;
+
+            if(Double.doubleToRawLongBits(excess) == 0){
+                // this means new position falls within current leg
+                currentPosition.setDistanceFromStart(distanceFromStart);
+                currentPosition.setLeg(currentLeg);
+                //Use the new position calcuation method in NavUtils
+                Point newPosition = NavUtils.getPosition(currentLeg.getStartPosition(), distanceFromStart, currentLeg.getHeading());
+                currentPosition.setPosition(newPosition);
+                return;
+            }
+            distanceFromStart = excess;
+        }
+
+        setStartPosition();
+    }
+
+    // Position running location at start of path
+    public void setStartPosition(){
+        currentPosition = new PositionInfo();
+        currentPosition.setRunningId(this.runningId);
+        Leg leg = legs.get(0);
+        currentPosition.setLeg(leg);
+        currentPosition.setPosition(leg.getStartPosition());
+        currentPosition.setDistanceFromStart(0.0);
+    }
+
+    public void setSpeed(double speed){
+        this.speedInMps = speed;
+    }
+
+    public double getSpeed(){
+        return this.speedInMps;
+    }
+
+    public synchronized void cancel(){
+        this.cancel.set(true);
+    }
+}
